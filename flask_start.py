0023dad9b2c10a29bf70a8d29116fec7a2c14409
Mandy C Souza <MandyCSouza@MacBook-Air.local>
2015-12-19 16:40:50 -0800

    Added CSS to Flask code.

--- flask_start.py
+++ flask_start.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__) 
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/hello/<msg>')
+def hello(msg):
+    msg="lalala"
+    return render_template('hello.html', msg=msg)
 
 if __name__=='__main__':
     app.run(debug=True,port=8000)
